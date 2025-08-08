a919cce2bd6cd6794403abbb0d17ab173a31e499
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-08 23:08:45 +0530

    dummy text removed from app.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,7 @@ const App = () => {
       </Routes>
       <Navbar />
 
-      <i className="fa-solid fa-house-user"></i>
-      <h3>Welcome to My App</h3>
-      <h1>DeepShare - Your Gateway to Collaborative Learning</h1>
-      <p>DeepShare is a platform designed to enhance collaborative learning experiences.</p>
+     
     </>
   )
 };
