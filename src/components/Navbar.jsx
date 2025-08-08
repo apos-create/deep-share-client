e926f7a1f4830a08da7fae1ad9563894c05c2198
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-08 22:14:48 +0530

    navbar styled.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,21 +6,30 @@ const Navbar = () => {
     return (
         <div className='navbar'>
 
-
-
             <div className="left-navbar">
                 <div className="logo">
                     <img src="https://t3.ftcdn.net/jpg/01/39/63/94/360_F_139639477_56w1dz3qNqCWlVEJvnXMjjs8qNHmQxTM.jpg" alt="DeepShare Logo" />
                 </div>
-            </div>
-            <ul className="nav-items">
-                <div className="nav-item">
-                    <NavLink>
-                        <i class="fa-solid fa-home"></i>
-                        <span>Home</span>
-                    </NavLink>
+
+                <div className="search-box">
+                    <input type="text" placeholder='Search here' />
                 </div>
-            </ul>
+            </div>
+
+            <div className="right-navbar">
+                <NavLink to={'/'}>
+                    <span>Home</span>
+                </NavLink>
+                <NavLink to={'/people'}>
+                    <span>People</span>
+                </NavLink>
+                <NavLink to={'/notifications'}>
+                    <span>Notifications</span>
+                </NavLink>
+                <NavLink to={'/my-profile'}>
+                    <span>My Profile</span>
+                </NavLink>
+            </div>
         </div>
     )
 }
