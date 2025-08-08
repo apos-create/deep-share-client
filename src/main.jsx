6c0d665b970c13a3684e72d4cefadc594000cd2c
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-08 21:07:41 +0530

    main.jsx error resolved.

--- src/main.jsx
+++ src/main.jsx
@@ -12,16 +12,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </React.StrictMode>
 );
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
