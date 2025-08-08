a4a43c04959039f558d24b36317d86bb4276cd1b
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-08 11:31:12 +0530

    font awesome configuration done.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
 
   return (
     <>
+    <i className="fa-solid fa-house-user"></i>
       <h3>Welcome to My App</h3>
       <h1>DeepShare - Your Gateway to Collaborative Learning</h1>
       <p>DeepShare is a platform designed to enhance collaborative learning experiences.</p>
