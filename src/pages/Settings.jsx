91fdab6611edfe8ab03bdc6933b26ccd39c3c6a7
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-10 09:12:52 +0530

    my profile card created.

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,19 +1,41 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import { useSelector } from 'react-redux'
+import ProfileIcon from '../components/ProfileIcon'
+import CoverImage from '../components/CoverImage'
 
 const Settings = () => {
+  const { user } = useSelector((state) => state.user)
   return (
     <>
-    <Navbar/>
-    
-    <div className="container">
-    <div className="left-content">
+      <Navbar />
 
-    </div>
-    <div className="middle-content"></div>
-    <div className="right-content"></div>
-   </div>
-   </>
+      <div className="container">
+        <div className="left-content">
+          <div className="my-profile-card">
+
+            <div className="cover-img-wrap">
+              <CoverImage clsName={"my-card-cover"} />
+
+              <div className="my-dp">
+                <ProfileIcon height={90} />
+              </div>
+            </div>
+
+           
+            <div className="my-card-content">
+              <h2>{user.fullName}</h2>
+              <p>Be Yourself!</p>
+              <p>India</p>
+              <p>Chandigarh University</p>
+            </div>
+
+          </div>
+        </div>
+        <div className="middle-content"></div>
+        <div className="right-content"></div>
+      </div>
+    </>
   )
 }
 
