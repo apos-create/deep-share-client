608cbbf763e2a1dd46ba0f3969f17bfd1fc39745
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-11 10:04:55 +0530

    home page updated.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,59 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import "../css/home.css"
-
+import { useSelector } from 'react-redux'
+import ProfileIcon from '../components/ProfileIcon'
+import CoverImage from '../components/CoverImage'
+import ConnectCard from '../components/ConnectCard'
+import connectData from '../assets/connectData'
+import { NavLink } from 'react-router-dom'
 
 const Home = () => {
+  const { user } = useSelector((state) => state.user);
+
   return (
     <>
-     <Navbar />
-    
-   
+      <Navbar />
 
-   
-   <h1>home</h1>
+
+      <div className="container">
+        <div className="left-content">
+          <div className="my-profile-card">
+
+            <div className="cover-img-wrap">
+              <CoverImage clsName={"my-card-cover"} />
+
+              <div className="my-dp">
+                <ProfileIcon height={90} />
+              </div>
+            </div>
+
+
+            <div className="my-card-content">
+              <h2>{user.fullName}</h2>
+              <p>Be Yourself!</p>
+              <p>India</p>
+              <p>Chandigarh University</p>
+            </div>
+
+          </div>
+        </div>
+
+        <div className="middle-content"></div>
+
+        <div className="right-content">
+
+          {
+            connectData.slice(6, 8).map((cd) => <ConnectCard data={cd} />)
+
+          }
+
+
+        </div>
+
+      </div>
+
+
     </>
   )
 }
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,55 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import { useSelector } from 'react-redux'
-import ProfileIcon from '../components/ProfileIcon'
-import CoverImage from '../components/CoverImage'
-import ConnectCard from '../components/ConnectCard'
-import connectData from '../assets/connectData'
-import { NavLink } from 'react-router-dom'
+
 
 
 const Settings = () => {
-  const { user } = useSelector((state) => state.user)
   return (
     <>
       <Navbar />
 
-      <div className="container">
-        <div className="left-content">
-          <div className="my-profile-card">
-
-            <div className="cover-img-wrap">
-              <CoverImage clsName={"my-card-cover"} />
-
-              <div className="my-dp">
-                <ProfileIcon height={90} />
-              </div>
-            </div>
-
-
-            <div className="my-card-content">
-              <h2>{user.fullName}</h2>
-              <p>Be Yourself!</p>
-              <p>India</p>
-              <p>Chandigarh University</p>
-            </div>
-
-          </div>
-        </div>
-
-        <div className="middle-content"></div>
-
-        <div className="right-content">
-        
-         {
-          connectData.slice(6,8).map((cd)=> <ConnectCard data={cd} />)
-        
-         }
-       
-
-        </div>
-
-      </div>
+      
     </>
   )
 }
