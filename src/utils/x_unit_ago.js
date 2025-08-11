4c3a28a31fea38ccf4912b8365232e245e36cfcc
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-11 22:52:25 +0530

    x_unit_ago time function created.

---
+++ src/utils/x_unit_ago.js
@@ -0,0 +1,44 @@
+const x_unit_ago = (date) => {
+    const now = new Date();
+    const past = new Date(date);
+
+    const diffInSeconds = Math.floor((now - past) / 1000);
+
+    const units = [
+        {
+            name: "year",
+            seconds: 31536000
+        },
+        {
+            name: "month",
+            seconds: 2592000
+        },
+        {
+            name: "day",
+            seconds: 86400
+        },
+        {
+            name: "hr",
+            seconds: 3600
+        },
+        {
+            name: "min",
+            seconds: 60
+        },
+        {
+            name: "sec",
+            seconds: 1
+        }
+    ];
+
+    for (const unit of units) {
+        const count = Math.floor(diffInSeconds / unit.seconds);
+        if (count >= 1) {
+            return `${count} ${unit.name}${count > 1 ? "s" : ""} ago`;
+        }
+    }
+
+    return "just now"
+};
+
+export {x_unit_ago};
