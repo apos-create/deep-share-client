c5c2ae91425c8eff3774e7d4ab12fee3a4e454fb
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-12 11:05:07 +0530

    navbar-mini-top created.

---
+++ src/components/NavbarMiniTop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NavbarMiniTop = () => {
+  return (
+    <div className='navbar-mini-top'>
+        <h2>DeepShare</h2>
+        <p>Search Here</p>
+    </div>
+  )
+}
+
+export default NavbarMiniTop
